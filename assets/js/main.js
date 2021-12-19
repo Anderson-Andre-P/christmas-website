fff2563d69d63e2bfd1001bf5b3d448d8fb1ec93
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-19 11:42:16 -0300

    feat: change dark/light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
